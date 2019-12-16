cb1cb1405cb1edb2f3aea9b5aa2105ba4f3664c0
Oleg Titov <oleggtitoff@gmail.com>
2019-12-16 06:31:33 +0200

    In Main added getting user bean from application context.

--- src/main/java/ua/training/Main.java
+++ src/main/java/ua/training/Main.java
@@ -1,9 +1,21 @@
 package ua.training;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ua.training.model.entity.User;
+
 public class Main {
 
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext context
+                = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        User user = context.getBean("user", User.class);
+
+        System.out.println(user.getName());
+        System.out.println(user.getSurname());
+        System.out.println(user.getAge());
+
+        context.close();
     }
 
 }
